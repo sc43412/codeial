7ec21895210c174b2de30b57ca4ebecd722343a2
sc43412 <sc43412@gmail.com>
2021-12-18 21:06:08 +0530

    create express server and install express

---
+++ index.js
@@ -0,0 +1,19 @@
+// require express
+const express = require("express");
+
+// fire up the server
+const app = express();
+
+// define port number
+const port = 8000;
+
+
+/// listen the port no using a callback function
+app.listen(port, function(err) {
+    // using interpolation to print 
+    // console.log("error", err); // normal method using comma to print err
+    // next line shows interpolation embeddd variable into string
+    // console.log('hello : ${2+2}');  ouput = hello : 4
+    if (err) { console.log('error : ${err}'); }
+    console.log('server is running successfully on port', port);
+})
